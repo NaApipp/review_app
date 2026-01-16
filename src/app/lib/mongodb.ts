bfd9fea7b8d2d1986965a27f3f55944f992f375d
NabilApipp <nabilapipp@gmail.com>
2026-01-16 22:55:46 +0700

    create main connection MongoDB to Project

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -7,7 +7,7 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client: MongoClient;
+let client: MongoClient;  
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
