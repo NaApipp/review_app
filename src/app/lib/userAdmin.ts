fa2c981e1a2754a301522b00b50204fd6dfaabfc
NabilApipp <nabilapipp@gmail.com>
2026-01-16 22:53:06 +0700

    mock (hardcore) data user for login

--- src/app/lib/userAdmin.ts
+++ src/app/lib/userAdmin.ts
@@ -7,15 +7,5 @@ export type AdminUser = {
 
 export const USERS: AdminUser[] = [
   { username: "langitmerah", password: "kopiSenja27" , role: "admin" },
-  { username: "bayu_nusantara", password: "anginTimur88", role: "admin" },
-  { username: "aksara_jawa", password: "hurufKuno19", role: "admin" },
-  { username: "senja_timur", password: "matahariTerbit", role: "admin" },
-  { username: "pandan_wangi", password: "daunHijau45", role: "admin" },
-  { username: "cakrawala_id", password: "langitBiru90", role: "admin" },
-  { username: "rembulan_selatan", password: "bulanPurnama7", role: "admin" },
-  { username: "tunasbangsa", password: "merdeka1945", role: "admin" },
-  { username: "sawah_hijau", password: "padiMenguning3", role: "admin" },
-  { username: "ombak_laut", password: "samudraBesar62", role: "admin" },
-  { username: "bhinneka", password: "ragamSatu99", role: "admin" },
 ];
 
