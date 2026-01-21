278bef0d9bf884751c82b44512fb55a93b43c7d6
NabilApipp <nabilapipp@gmail.com>
2026-01-21 20:18:51 +0700

    update style sector handphone and laptoop

--- src/app/handphone-sector/page.tsx
+++ src/app/handphone-sector/page.tsx
@@ -9,34 +9,84 @@ export default function OnBoardingPage() {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen gap-6 bg-[#1A3D64] text-[#F4F4F4]">
         {/* Headline */}
-        <h1 className="md:text-[1.3rem] text-center font-bold">
+        <h1 className="md:text-[1.3rem] text-center font-bold">Selamat Datang Di <span className="text-[#00F7FF] font-extrabold">Handphone</span> Sector</h1>
+        <h1 className="md:text-[10px] text-center mt-5 font-bold">
           <TypeAnimation
             sequence={[
-              "Selamat Datang Di Handphone Sector",
-              1000,
               "Silahkan Pilih Menu Login Dibawah",
               1000,
             ]}
             wrapper="span"
             speed={50}
-            className="text-[1.3rem] font-bold"
+            className="text-[13px] font-bold"
             repeat={Infinity}
           />
         </h1>
-        <div className="flex gap-4 md:mt-0 mt-7">
+        <div className="flex gap-7 md:mt-0 mt-7">
+          {/* Reviwe Card */}
           <Link
             href={"/login-admin"}
-            className="bg-[#1D546C] p-4 rounded-lg text-[#F4F4F4]"
+            className="bg-[#26476C] border border-[#2b507a] rounded-2xl p-5 flex flex-col gap-2 w-80 transition-transform duration-300 hover:scale-110 cursor-pointer"
           >
-            Login Dashboard
+            <div
+              className="bg-[#305073] w-min flex items-center justify-center border-[#36659b] border rounded-[40%] p-5"
+              id="icon"
+            >
+              <svg
+                className="w-10 h-10 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9.143 4H4.857A.857.857 0 0 0 4 4.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 10 9.143V4.857A.857.857 0 0 0 9.143 4Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 20 9.143V4.857A.857.857 0 0 0 19.143 4Zm-10 10H4.857a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286A.857.857 0 0 0 9.143 14Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286a.857.857 0 0 0-.857-.857Z"
+                />
+              </svg>
+            </div>
+            <h1 className="mt-5 font-bold">Login Dashboard</h1>
+            <h1 className="mt-5 font-light">
+            Manajemen produk untuk pengalaman pelanggan yang lebih baik.{""}
+            </h1>
           </Link>
+          {/* Reviwe Card */}
           <Link
             href={"/login-handphone"}
-            className="bg-[#1D546C] p-4 rounded-lg text-[#F4F4F4]"
+            className="bg-[#26476C] border border-[#2b507a] rounded-2xl p-5 flex flex-col gap-2 w-80 transition-transform duration-300 hover:scale-110 cursor-pointer"
           >
-            Login Review
+            <div
+              className="bg-[#305073] w-min flex items-center justify-center border-[#36659b] border rounded-[40%] p-5"
+              id="icon"
+            >
+              <svg
+                className="w-10 h-10 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2h-6.616l-2.88 2.592C8.537 20.461 7 19.776 7 18.477V17H5a2 2 0 0 1-2-2V6Zm4 2a1 1 0 0 0 0 2h5a1 1 0 1 0 0-2H7Zm8 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2Zm-8 3a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H7Zm5 0a1 1 0 1 0 0 2h5a1 1 0 1 0 0-2h-5Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <h1 className="mt-5 font-bold">Login Review</h1>
+            <h1 className="mt-5 font-light">
+              Berikan ulasan terbaik Anda dan lihat penilaian pelanggan lain terhadap produk kami.{" "}
+            </h1>
           </Link>
         </div>
+        <Link href={"/on-boarding"} className="bg-white p-5 rounded-2xl font-bold text-[#2b507a]  transition-transform duration-300 hover:scale-105">Kembali ke Halaman Utama</Link>
       </div>
     </>
   );
