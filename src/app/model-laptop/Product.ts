cf5f67bf60936eaffbbe3fc212b08872e6bf9a06
NabilApipp <nabilapipp@gmail.com>
2026-01-21 15:24:43 +0700

    create model for laptop model

---
+++ src/app/model-laptop/Product.ts
@@ -0,0 +1,20 @@
+// import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IProduct extends Document {
+  productId: string;
+  produkName: string;
+  platform: string;
+}
+
+const ProductSchema = new Schema<IProduct>(
+  {
+    productId: { type: String, required: true, unique: true },
+    produkName: { type: String, required: true },
+    platform: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Product ||
+  mongoose.model<IProduct>("Laptop", ProductSchema);
---
+++ src/app/model-laptop/Review.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IReview extends Document {
+  productId: string;
+  rating: number;
+  review: string;
+  reviewer: string;
+}
+
+const ReviewSchema = new Schema<IReview>(
+  {
+    productId: {
+      type: String,
+      required: true,
+      index: true
+    },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    review: { type: String, required: true },
+    reviewer: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Review ||
+  mongoose.model<IReview>("Reviews_laptop", ReviewSchema);
