4f8a03672196fd6bdea0ad03f612ceb6efb82a56
NabilApipp <nabilapipp@gmail.com>
2026-01-21 10:20:55 +0700

    fixing direct logout /on-boarding to /login

--- src/app/(main)/beranda/components/Header.tsx
+++ src/app/(main)/beranda/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
         <div className="flex md:flex-row md:gap-0 flex-col gap-3 items-center justify-between mb-5">
         <h1 className="md:text-2xl font-bold">Daftar Produk</h1>
         <Link
-          href="/on-boarding"
+          href="/login"
           onClick={logout}
           className="block py-2 px-3 bg-[#1D546C] hover:bg-[#1D546C]/60 text-white text-heading rounded hover:bg-neutral-tertiary  md:p-3"
         >
