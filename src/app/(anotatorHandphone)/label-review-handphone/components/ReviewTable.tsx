23c4bdbbffdf80565b00a0bfafc2cb8b6142194e
NabilApipp <nabilapipp@gmail.com>
2026-01-26 10:34:46 +0700

    fixing style anotator laptop

--- src/app/(anotatorHandphone)/label-review-handphone/components/ReviewTable.tsx
+++ src/app/(anotatorHandphone)/label-review-handphone/components/ReviewTable.tsx
@@ -5,26 +5,23 @@ import { ReviewRow } from "./ReviewRow";
 export default function ReviewTable({ reviews }: { reviews: any[] }) {
   return (
     <div className="bg-[#1A3D64]">
-      <table className="w-full border text-center">
-      <thead className="border">
-        <tr className="border">
-          <th className="pt-5 pb-5">Review Id</th>
-          <th className="pt-5 pb-5">Review</th>
-          <th className="pt-5 pb-5">Label</th>
-          <th className="pt-5 pb-5">Labeled By</th>
-          <th className="pt-5 pb-5">Action</th>
-        </tr>
-      </thead>
+      <table className="w-full border-collapse rounded-xl shadow-sm overflow-hidden  text-center">
+        <thead className="bg-slate-100">
+          <tr className="text-slate-700 text-sm uppercase tracking-wide">
+            <th className="pt-5 pb-5">Review Id</th>
+            <th className="pt-5 pb-5">Review</th>
+            <th className="pt-5 pb-5">Label</th>
+            <th className="pt-5 pb-5">Labeled By</th>
+            <th className="pt-5 pb-5">Action</th>
+          </tr>
+        </thead>
 
-      <tbody>
-        {reviews.map((review) => (
-          <ReviewRow
-            key={review.reviewId}
-            review={review}
-          />
-        ))}
-      </tbody>
-    </table>
+        <tbody>
+          {reviews.map((review) => (
+            <ReviewRow key={review.reviewId} review={review} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
