5fe9d87465d3ee72c35c356e5cf11855ce0e84ad
NabilApipp <nabilapipp@gmail.com>
2026-02-03 13:34:21 +0700

    delete one user and migrate to v2

--- src/app/lib/userAnotatorV2.ts
+++ src/app/lib/userAnotatorV2.ts
@@ -1,13 +1,13 @@
 // lib/users.ts
-export type AnotatorUser = {
+export type AnotatorUserV2 = {
   username: string;
   password: string; // demo only (plaintext). Jangan gini di production.
   role: "anotator";
 };
 
-export const USERS: AnotatorUser[] = [
-  { username: "pandan_wangi", password: "daunHijau45" , role: "anotator" },
-  // { username: "cakrawala_id", password: "langitBiru90" , role: "anotator" },
+export const USERS: AnotatorUserV2[] = [
+  // { username: "pandan_wangi", password: "daunHijau45" , role: "anotator" },
+  { username: "cakrawala_id", password: "langitBiru90" , role: "anotator" },
 
   // SUPER ADMIN
   { username: "gajahkayang", password: "gajahsulap" , role: "anotator" },
